405c214ba530ba9547a70489684c7bbbabb805de
Sanjith T <sanjithnathan@gmail.com>
2022-03-08 21:16:17 +0530

    Delete connection.js

--- connection.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-// var img = require('./img');
-
-const connection = function(){
-    mongoose.connect('mongodb://localhost/testImage');
-    console.log('connected to mongodb');
-}
-module.exports = connection;
